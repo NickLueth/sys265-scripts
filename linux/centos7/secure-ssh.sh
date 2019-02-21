4cc25d2a1b41494b7a81c49d333cda59c110decf
nickranger44 <nicholaslueth@gmail.com>
2019-02-21 11:24:47 -0500

    Fixed secure-ssh.sh file

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+#SECURE-SSH.SH
+#AUTHOR NICKRANGER44
+#Creates a new ssh user called $1
+#Adds a public key to that users authorized keys file
+#Removes roots ability to ssh in
+
 echo "testing 123"
